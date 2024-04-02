7c4050888e9beb69a8673dd07887a5b774f64aca
dev5212512 <dev5212512@gmail.com>
2024-04-02 23:46:49 +0500

    Make use of state to select element in a list

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+
 function ListGroup() {
+  const [selectedIndex, setSelectedIndex] = useState(0);
   const cities = ["Tokoyo", "Paris", "London", "New York"];
+
+  const handleClick = (index: number) => {
+    setSelectedIndex(index);
+  };
   return (
     <>
       <h1>List of Cities</h1>
       <ul className="list-group">
-        {cities.map((city) => (
-          <li className="list-group-item" key={city}>
+        {cities.map((city, index) => (
+          <li
+            className={
+              selectedIndex == index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={index}
+            onClick={() => handleClick(index)}
+          >
             {city}
           </li>
         ))}
